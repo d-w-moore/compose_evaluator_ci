aaab3ea0910bf5613eaa81f2c3344db01303e2d7
d-w-moore <dmoore@renci.org>
2020-10-29 14:50:13 -0400

    evaluate to PARAMETER_EXPR

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM ubuntu:18.04
 RUN  apt update 
 RUN  apt install -y python
-ARG  param_expr
-ENV  EXPR_TO_EVAL="${param_expr}"
 COPY eval.py /
-CMD  python /eval.py $EXPR_TO_EVAL /output/result
+CMD  python /eval.py "$PARAMETER_EXPR" /output/result
--- eval.py
+++ eval.py
@@ -3,14 +3,14 @@ from __future__ import print_function
 import sys, os
 import math
 
-arg_to_eval = sys.argv[1]
+SINGLE_QUOTE="'"
+DOUBLE_QUOTE='"'
 
-if arg_to_eval.startswith("EXPR"): arg_to_eval = os.environ.get(arg_to_eval,'1234')
+arg_to_eval = os.environ["PARAMETER_EXPR"]
 
-arg_to_eval = arg_to_eval.strip("'" '"')
+arg_to_eval = arg_to_eval.strip(SINGLE_QUOTE+DOUBLE_QUOTE)
 
-log_file = sys.stdout \
-                      if not sys.argv[2:] \
+log_file = sys.stdout  if not sys.argv[2:] \
            else open(sys.argv[2],'w')
 
 print('eval({!r}) -> {!r}'.format(arg_to_eval, eval(arg_to_eval)), file = log_file)
