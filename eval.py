e678a79968375746cf801e3a43761cdc9b1ab622
d-w-moore <dmoore@renci.org>
2020-10-29 13:08:51 -0400

    strip quotes if necessary

--- eval.py
+++ eval.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys, os
+import math
 
 arg_to_eval = sys.argv[1]
 
 if arg_to_eval.startswith("EXPR"): arg_to_eval = os.environ.get(arg_to_eval,'1234')
+
+arg_to_eval = arg_to_eval.strip("'" '"')
 
 log_file = sys.stdout \
                       if not sys.argv[2:] \
